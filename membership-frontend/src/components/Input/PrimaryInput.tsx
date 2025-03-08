cf61541d4e7f76d01b3f7894b0c027a46370aa0a
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-03-08 20:06:18 -0300

    create frontend

---
+++ membership-frontend/src/components/Input/PrimaryInput.tsx
@@ -0,0 +1,29 @@
+import { Input } from "@chakra-ui/react";
+import { ChangeEventHandler } from "react";
+
+interface PrimaryInputProps {
+  name: string;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  label: string;
+}
+
+export default function PrimaryInput({
+  name,
+  value,
+  onChange,
+  label,
+}: PrimaryInputProps) {
+  return (
+    <div className="input-container">
+      <label className="label">{label}</label>
+      <Input
+        variant="filled"
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
